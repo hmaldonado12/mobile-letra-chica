737fbccf37fb957085b0e9189149a5c1b55d3259
Fernando Laudadio <fn.laudadio@gmail.com>
2025-05-18 19:14:25 -0300

    Using login page

--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Added to support custom elements
+  imports: [
+    FormsModule,
+    IonicModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginPage {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  onLogin() {
+    this.router.navigate(['/home']);
+  }
 }
