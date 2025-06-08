0cc198917574ee94f626fa213ab4c703153c498d
hmaldonado12 <hmaldonado@fi.uba.ar>
2025-06-08 10:36:09 -0300

    feat(new-contract): implement file upload functionality for PDF documents

--- src/app/page/new-contract/new-contract.page.ts
+++ src/app/page/new-contract/new-contract.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import {DocumentAnalysisRepositoryService} from "../../infra/rest/document-analysis-repository.service";
 
 @Component({
   selector: 'app-new-contract',
@@ -9,6 +10,28 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule]
 })
 export class NewContractPage {
+
+  constructor(private documentAnalysisRepo: DocumentAnalysisRepositoryService) {}
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      if (file.type === 'application/pdf') {
+        this.documentAnalysisRepo.analyzeDocument(file).subscribe({
+          next: (response) => {
+            console.log('Respuesta del backend:', response.candidates[0].content.parts[0].text);
+          },
+          error: (err) => {
+            console.error('Error al analizar el documento:', err);
+          }
+        });
+      } else {
+        console.error('Por favor selecciona un archivo PDF.');
+      }
+    }
+  }
+
   onUpload() {
     console.log('Choose file');
   }
